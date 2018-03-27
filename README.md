bb57fb12a04345375ef4cfff6ca1c106309af4ad
bs317482 <bernardbnard@gmail.com>
2018-03-27 16:35:03 -0400

    I have circles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Connect4
 Project #1 game
+https://media1.giphy.com/media/v0MO5isRawUJW/giphy.gif
+
+
+<!-- var container =  -->
---
+++ script.js
@@ -0,0 +1,15 @@
+// function appendRow() {
+//     var tbl = document.getElementById9('board')
+//     row = tbl.insertRow(tbl.rows.length),i;
+// }
+    
+// var connect = $('row');
+// for(i =0; i > 7; i++) {
+//     connect.append(<div class = 'row'></div>)
+// 
+
+
+function myFunction() {
+    var x = document.getElementById("rows").rows.length;
+    document.getElementById("demo").innerHTML = "Found " + x + " tr elements in the table.";
+}
