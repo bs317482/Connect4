2696c5b8a1f908774c566991b7869df1c15056f3
bs317482 <bernardbnard@gmail.com>
2018-03-28 17:15:17 -0400

    JAVASCRIPT

--- script.js
+++ script.js
@@ -4,39 +4,19 @@
 //   .click(
 //     function(){
 //      alert('Click event is fired');
-//     }
-//   )
-//   .hover(
-//     function(){
-//       $(this).css('background', 'black');
-//     },
-//     function(){
-//       $(column).css('background', 'red');
-//     }
-//   );
-// function elemen
-// for (i = 0; i < columns.length; i++) { 
-//     text += [i] + "<br>";
-// }
+//     
 
-// function
-// <div class="column">1</div>
-// function myFunction() {
-//     document.getElementById("1").click();
-// }
-// console.log(go)
 
-// var favoriteMovies = ['The Five Obstructions', 'The Triplets of Belleville', 'About Time', 'Two Weeks Notice'];
-
-// console.log('My favorite movies are: ');
-// for (var i = 0; i < favoriteMovies.length; i++) {
-//   console.log([i + 1] + ". " + favoriteMovies[i]);
-// }
-var toggleColor = (function(){
-    var currentColor = "white";
- 
-     return function(){
-         currentColor = currentColor == "white" ? "magenta" : "white";
-         d3.select(this).style("fill", currentColor);
-     }
- })();
+Let PlayerOneTurn = true
+const PlaceGamePiece = function(divtochange) {
+    if(PlayerOne){
+        change divtochange to Red 
+        PlayerOne = false
+    } else {
+        change divtochange to black 
+        PlayerOneTurn = True
+    }
+}
+$(.1).on('click',function()){
+    PlaceGamePiece(event.target.id)
+})
