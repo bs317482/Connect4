37c4921ce1252ca2f0a385a02b0db42d1ce284af
bs317482 <bernardbnard@gmail.com>
2018-03-29 15:17:31 -0400

    java

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 $('.1').on('click', function () {
     placeGamePiece(event.target.id)
 })
-let banana = [0, "text", 77, 100 ]
+// let banana = [0, "text", 77, 100 ]
 
-console.log(banana)
+// console.log(banana)
 
 let color = ['red', 'yellow']
 
 let piece = 0
 
+
+
 $('.piece').click(function () {
     let column = $(this).parent().prop("id")
-    console.log("column", column)
-    console.log("testing stuff", banana[column])
-    console.log(banana)
+    // console.log("column", column)
+    // console.log("testing stuff", banana[column])
+    // console.log(banana)
 
     if (piece % 2 == 0) {
         $(this).css('background', color[0])
